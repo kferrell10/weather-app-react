d5c7ba3c7be85ab22d777dac2c246ec2bcd81cac
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-03 16:56:31 -0800

    Update App.js
    add credit links to page bottom

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World!</h1>
         <Weather city="San Diego" />
+        <small><a href="https://confident-montalcini-2e4e53.netlify.app/" target="_blank">Open-source code</a> by <a href="https://kimferrelldesign.com/" target="_blank">Kim Ferrell</a></small>
       </header>
     </div>
   );
