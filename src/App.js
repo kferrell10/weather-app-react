203b81b73e0782647d0edbb600d84d1ce6c71b62
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-03 16:30:43 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <Weather city="San Diego" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in ${props.city} is ${Math.round(response.data.main.temp)}°C`);
+    }
+    let apiKey = "f3711ec096b8e2b5d745c777afc03d71";
+    let units = "metric";
+    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+    let apiUrl = `${apiEndpoint}?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return (<Loader type="Bars" color="#00BFFF" height={50} width={50} />)
+    
+}
