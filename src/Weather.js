ef64bb168bddd2da5638b7fc7bf6e1b2fc4df589
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-03 18:11:10 -0800

    retrieve working search form
    retrieve working search form (unstyled) test search and display function

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
 
-export default function Weather(props) {
-    function handleResponse(response) {
-        alert(`The weather in ${props.city} is ${Math.round(response.data.main.temp)}°C`);
-    }
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
     let apiKey = "f3711ec096b8e2b5d745c777afc03d71";
     let units = "metric";
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-    let apiUrl = `${apiEndpoint}?q=${props.city}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
 
-    axios.get(apiUrl).then(handleResponse);
-    return (<Loader type="Bars" color="orange" height={50} width={50} />)
-    
+  // https://api.openweathermap.org/data/2.5/weather?q=London&appid=f3711ec096b8e2b5d745c777afc03d71&units=Metric
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="type a city name"
+        autoFocus={true}
+        onChange={updateCity}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul className="weatherReport">
+          <li>
+            <strong>The weather in {city} is:</strong>
+          </li>
+          <li>Temperature: {Math.round(weather.temperature)}˚C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
