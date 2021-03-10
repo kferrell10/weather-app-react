434a1d6b38d0e78250b92542d0ca4ec4fcd50bfc
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-10 11:45:44 -0800

    Update Weather.js
    clean up weather(props)

--- src/Weather.js
+++ src/Weather.js
@@ -14,9 +14,9 @@ export default function Weather(props) {
       city: response.data.name,
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
-      wind: response.data.wind,
+      wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
     });
   }
