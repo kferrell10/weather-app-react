32c57e7cbcf3a8aeff420064057d464a2aba7cd8
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-10 12:50:41 -0800

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
-      icon: response.data.weather[0].icon,
+      icon: (`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`),
       description: response.data.weather[0].description,
       wind: response.data.wind,
       humidity: response.data.main.humidity,
@@ -27,7 +27,7 @@ export default function Weather(props) {
   }
 
   // https://api.openweathermap.org/data/2.5/weather?q=London&appid=f3711ec096b8e2b5d745c777afc03d71&units=Metric 
-  // `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+  // `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`, previously rendered icon in setWeather!
 
   function updateCity(event) {
     setCity(event.target.value);
