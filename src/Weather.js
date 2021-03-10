b29b48909be1ed5ece0f46f1ce71a43205eb0b04
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-10 11:20:52 -0800

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
-      wind: response.data.wind.speed,
+      wind: response.data.wind,
       humidity: response.data.main.humidity,
     });
   }
