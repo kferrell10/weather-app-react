4f231729181396a6d0e85cadf9cb6c2f217276a7
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-10 20:28:06 -0800

    Update WeatherInfo.js
    round wind speed

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -23,7 +23,7 @@ export default function WeatherInfo(props) {
               <div className="col-12">  
                 <ul className="weatherReport">
                   <li>Humidity: {props.data.humidity}%</li>
-                  <li>Wind: {props.data.wind}km/h</li>
+                  <li>Wind: {Math.round(props.data.wind)} km/h</li>
                 </ul>
               </div>
           </div>
