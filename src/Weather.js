b24d3e62a7b59e38bf8b59a29b33596be914bf4d
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-10 20:01:52 -0800

    Update Weather.js
    add speed to wind:

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
-      wind: response.data.wind,
+      wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       city: response.data.name,
     });
